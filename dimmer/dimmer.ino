ea2858ffeb6cd79ca14a4b140c300733bef78deb
GuilhermeMauro <guilherme.oferraz@yahoo.com.br>
2015-07-11 16:27:36 -0300

    First test with Zerocrossing Detector.

--- dimmer/dimmer.ino
+++ dimmer/dimmer.ino
@@ -1,12 +1,18 @@
 int zerocrossing_sensor = 7;
 int trigger = 6;
+int tempo = 0;
+int trash;
 void setup(){
   pinMode(zerocrossing_sensor, INPUT);
   pinMode(trigger, OUTPUT);
+  Serial.begin(9600);
 }
 void loop(){
-  while(digitalRead(zerocrossing_sensor) != 0){}
-  
-  
+  //wait for passage of zero crossing
+  if(digitalRead(zerocrossing_sensor) == 0){}
+  trash = millis() - tempo;
+  tempo = millis();
+  Serial.println(trash);
+  delayMicroseconds(100);
 }
 
