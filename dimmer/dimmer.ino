e80002332b17de5f95f2c2a4c29d3e7fe182c618
GuilhermeMauro <guilherme.oferraz@yahoo.com.br>
2015-07-12 19:47:58 -0300

    Potentiometer added in source. Now its possible controler the dimmer with potentiometer.

--- dimmer/dimmer.ino
+++ dimmer/dimmer.ino
@@ -1,5 +1,9 @@
 int zerocrossing_sensor = 7;
 int trigger = 6;
+//potentiometer pin
+int pino_potent = A0;
+//potentiometer value
+int potent = 0;
 void setup(){
   pinMode(zerocrossing_sensor, INPUT);
   pinMode(trigger, OUTPUT);
@@ -8,9 +12,11 @@ void setup(){
 }
 void loop(){
   //wait for passage of zerocrossing
-  if(digitalRead(zerocrossing_sensor) != 0){
+   if(digitalRead(zerocrossing_sensor) != 0){
+  potent = analogRead(pino_potent);
+  potent =  map(potent, 0, 1023, 0, 111);
   //expected time to turn on the MOC3021
-  delayMicroseconds(65*110);
+  delayMicroseconds(65*potent);
   digitalWrite(trigger, HIGH);
   //expected time to turn off the MOC3021
   delayMicroseconds(50);
