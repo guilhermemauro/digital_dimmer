4e243dd49b1fe7c1c9f379896ce06ec55531c8ca
GuilhermeMauro <guilherme.oferraz@yahoo.com.br>
2015-08-03 19:16:15 -0300

    The source is portable for Attiny85/45

--- dimmer/dimmer.ino
+++ dimmer/dimmer.ino
@@ -1,26 +1,32 @@
-int zerocrossing_sensor = 7;
-int trigger = 6;
+//*********************************
+//Firmware created by Guilherme Mauro
+// guilherme.oferraz@yahoo.com.br
+//*********************************
+
+int zerocrossing_sensor = 3;
+int trigger = 4;
 //potentiometer pin
-int pino_potent = A0;
+int pino_potent = A1;
 //potentiometer value
 int potent = 0;
 void setup(){
   pinMode(zerocrossing_sensor, INPUT);
   pinMode(trigger, OUTPUT);
-  Serial.begin(9600);
+  //pinMode(pino_potent,INPUT);
   digitalWrite(trigger, LOW);
 }
 void loop(){
   //wait for passage of zerocrossing
-   if(digitalRead(zerocrossing_sensor) != 0){
-  potent = analogRead(pino_potent);
-  potent =  map(potent, 0, 1023, 0, 111);
-  //expected time to turn on the MOC3021
-  delayMicroseconds(65*potent);
-  digitalWrite(trigger, HIGH);
-  //expected time to turn off the MOC3021
-  delayMicroseconds(50);
-  digitalWrite(trigger, LOW);
+  if(digitalRead(zerocrossing_sensor) != 0){
+    potent = analogRead(pino_potent);
+    potent =  map(potent, 0, 1023, 0, 111);
+    //expected time to turn on the MOC3021
+    delayMicroseconds(65*potent);
+    digitalWrite(trigger, HIGH);
+    //expected time to turn off the MOC3021
+    delayMicroseconds(50);
+    digitalWrite(trigger, LOW);
   }
 }
+
 
