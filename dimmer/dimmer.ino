fe6734409295f9600e35d59264ad2ebac6b0967c
GuilhermeMauro <guilherme.oferraz@yahoo.com.br>
2015-08-04 11:36:07 -0300

    Change the pins for use on the board scheme.

--- dimmer/dimmer.ino
+++ dimmer/dimmer.ino
@@ -3,23 +3,26 @@
 // guilherme.oferraz@yahoo.com.br
 //*********************************
 
-int zerocrossing_sensor = 3;
+// physical pin 6 in Attiny85
+int zerocrossing_sensor = 1;
+//physical pin 3 in Attiny85
 int trigger = 4;
-//potentiometer pin
-int pino_potent = A3;
+//potentiometer pin (pyshical pin 2 in Attiny85)
+int pino_potent = 3;
 //potentiometer value
 int potent = 0;
 void setup(){
   pinMode(zerocrossing_sensor, INPUT);
   pinMode(trigger, OUTPUT);
-  //pinMode(pino_potent,INPUT);
+  pinMode(pino_potent,INPUT);
   digitalWrite(trigger, LOW);
 }
 void loop(){
+
   //wait for passage of zerocrossing
   if(digitalRead(zerocrossing_sensor) != 0){
     potent = analogRead(pino_potent);
-    potent =  map(potent, 0, 1023, 116, 5);
+    potent =  map(potent, 0, 1023, 114, 5);
     //expected time to turn on the MOC3021
     delayMicroseconds(65*potent);
     digitalWrite(trigger, HIGH);
